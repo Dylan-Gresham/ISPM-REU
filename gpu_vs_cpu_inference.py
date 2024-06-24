a971a17566a7ba7d7ab0785354ab83e3fcca3926
DylanGresham <113629506+DylanGresham@users.noreply.github.com>
2024-06-23 23:18:59 -0600

    Fix CPU vs GPU files

--- gpu_vs_cpu_inference.py
+++ gpu_vs_cpu_inference.py
@@ -1,28 +1,75 @@
+# Most importantly, make sure you're in a Python virtual environment first, have
+# a Nvidia GPU, and have the CUDA toolkit installed.
+
+# Prior to installing `llama-cpp-python`, we'll need to do some extra steps. All
+# you'll need to do is set some additional CLI environment variables before 
+# executing the pip command to install libraries.
+
+
+# Windows
+
+# The commands to set the environment variables are:
+
+# - `$env:CMAKE_ARGS="-DLLAMA_CUBLAS=on"`
+# - `$env:FORCE_CMAKE=1`
+# - `$env:CUDACXX="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v<version_number>\bin\nvcc.exe"`
+#   - Change `<version_number>` to whichever version of the CUDA toolkit you have installed.
+#   - For instructions on downloading and installing the CUDA toolkit on 
+#     Windows, visit
+#     https://docs.nvidia.com/cuda/cuda-installation-guide-microsoft-windows/index.html.
+
+# Linux
+
+# The commands to set the environment variables are:
+
+# - `export CMAKE_ARGS="-DLLAMA_CUBLAS=on"`
+# - `export FORCE_CMAKE=1`
+# - `export CUDACXX="<path_to_nvcc_executable>"`
+#   - Where `<path_to_nvcc_executable>` is your installation path to the 
+#     executable for your Linux distribution's CUDA Toolkit installation.
+#   - For instructions on downloading and installing the CUDA toolkit on 
+#     Windows, visit
+#     https://docs.nvidia.com/cuda/cuda-installation-guide-linux/index.html.
+
+# Once the environment variables have been set, execute the following pip 
+# command to download and install the appropriately compiled `llama-cpp-python`
+# and other necessary packages.
+
+# pip install --upgrade --force-reinstall --no-cache-dir llama-cpp-python jupyter huggingface_hub
+
+# HuggingFace Hub Setup
+
+# You'll need to create a HuggingFace account and access token:
+
+# 1. Create an account on [HuggingFace](https://huggingface.co).
+# 2. Once logged into your account, click your profile picture in the upper 
+#    right corner and navigate to Settings > Access Tokens.
+# 3. Click New Token and generate a new token, I made mine a "Write" token but 
+#    it shouldn't matter if it's a "Read" or "Write" token for this script.
+# 4. Run the command `huggingface-cli login` and paste in the access token you 
+#    created in step 3.
+
+# Git & Git LFS Setup
+
+# 1. Download [git](https://git-scm.com/downloads) if you don't already have it 
+#    installed.
+#    1. Setup your git account and verify that you're able to clone a private 
+#       repo (doesn't matter if the repo actually has anything in it, just need 
+#       to make sure that you're able to use git properly).
+#    2. Follow the git-lfs install guide 
+#       https://github.com/git-lfs/git-lfs?utm_source=gitlfs_site&utm_medium=installation_link&utm_campaign=gitlfs#installing.
+#    3. If you didn't run it in the guide, run the command `git lfs install` 
+#       after getting git setup and git-lfs installed.
+
 # Import libraries
 import os
 from timeit import default_timer as timer
-from llama_cpp_python import Llama
-
-# Installing libraries:
-#   Most importantly, make sure you're in a Python virtual environment first, have an Nvidia GPU, and have
-#   the CUDA toolkit installed.
-#
-#   When installing llama-cpp-python, you'll need to do some extra steps beforehand.
-#   All you'll need to do is set some additional CLI args before executing the pip command. These args are:
-#
-#   - CMAKE_ARGS="-DLLAMA_CUBLAS=on"
-#   - FORCE_CMAKE=1
-#   - CUDACXX="C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v<version_number>\bin\nvcc.exe"
-#     - Change path to wherever your installation is.
-#
-#   Then execute the following pip command to install the appropriately compiled llama-cpp-python.
-#
-#   pip install --upgrade --force-reinstall --no-cache-dir llama-cpp-python
+from llama_cpp import Llama
 
 # Get model with GPU offloading
 model = Llama.from_pretrained(
-    repo_id="",
-    filename="",
+    repo_id="QuantFactory/Meta-Llama-3-8B-Instruct-GGUF-v2",
+    filename="Meta-Llama-3-8B-Instruct-v2.Q6_K.gguf",
     n_gpu_layers=-1,  # Offload as much as possible
     n_threads=os.cpu_count(),
     n_ctx=8192,
@@ -55,8 +102,8 @@ del model
 
 # Get model with no GPU offloading
 model = Llama.from_pretrained(
-    repo_id="",
-    filename="",
+    repo_id="QuantFactory/Meta-Llama-3-8B-Instruct-GGUF-v2",
+    filename="Meta-Llama-3-8B-Instruct-v2.Q6_K.gguf",
     n_threads=os.cpu_count(),
     n_ctx=8192,
     verbose=False,
@@ -90,15 +137,17 @@ if cpu_elapsed_time > gpu_elapsed_time:
     diff_elapsed = cpu_elapsed_time - gpu_elapsed_time
     diff_mins, diff_secs = divmod(diff_elapsed, 60)
     diff_hours, diff_mins = divmod(diff_mins, 60)
+    percentage_difference = ((cpu_elapsed_time - gpu_elapsed_time) / gpu_elapsed_time) * 100
 else:
     gpu_faster = False
     diff_elapsed = gpu_elapsed_time - cpu_elapsed_time
     diff_mins, diff_secs = divmod(diff_elapsed, 60)
     diff_hours, diff_mins = divmod(diff_mins, 60)
+    percentage_difference = ((gpu_elapsed_time - cpu_elapsed_time) / cpu_elapsed_time) * 100
 
 # Print results
-print(f"CPU-only took: {cpu_hours:.0f} hours, {cpu_mins:.0f} minutes, and {cpu_secs:.0f} seconds for inference.")
-print(f"With GPU offloading it took: {gpu_hours:.0f} hours, {gpu_mins:.0f}, and {gpu_secs:.0f} seconds for inference.")
+print(f"CPU-only took: {cpu_hours:.0f} hours, {cpu_mins:.0f} minutes, and {cpu_secs:.2f} seconds for inference.")
+print(f"With GPU offloading it took: {gpu_hours:.0f} hours, {gpu_mins:.0f} minutes, and {gpu_secs:.2f} seconds for inference.")
 print("CPU Output:")
 print()
 print(cpu_output['choices'][0]['message']['content'])
@@ -109,7 +158,8 @@ print(gpu_output['choices'][0]['message']['content'])
 print()
 
 if gpu_faster:
-    print(f"GPU inference time was faster by {diff_hours:.0f} hours, {diff_mins:.0f} minutes, and {diff_secs:.0f} seconds.")
+    print(f"GPU Inference time was {percentage_difference:.2f}% faster.")
+    print(f"GPU inference time was faster by {diff_hours:.0f} hours, {diff_mins:.0f} minutes, and {diff_secs:.2f} seconds.")
 else:
-    print(f"CPU inference time was faster by {diff_hours:.0f} hours, {diff_mins:.0f} minutes, and {diff_secs:.0f} seconds.")
-
+    print(f"CPU Inference time was {percentage_difference:.2f}% faster.")
+    print(f"CPU inference time was faster by {diff_hours:.0f} hours, {diff_mins:.0f} minutes, and {diff_secs:.2f} seconds.")
